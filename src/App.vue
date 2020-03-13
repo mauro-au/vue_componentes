6344d6107e0fdb3509fcc995f728b349fb7ca70a
mauriciodesign <mauricio.a.design@gmail.com>
2020-03-12 21:21:13 -0300

    Vue componentes

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="bg">
+        <div v-for="(personaje, index) in personajes" :key="index">
+            <TarjetaComponente v-bind:nombre="personaje.name" v-bind:imagen="personaje.image" />
+        </div>
+    </div>
+</template>
+
+<script>
+    import TarjetaComponente from "./TarjetaComponente.vue";
+    export default {
+    name: "App",
+    components: { 
+        TarjetaComponente 
+    },
+    data(){
+        return {
+            personajes:[
+                {
+                    name:'Rick Sanchez',
+                    image:'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+                },
+                {
+                    name:'Morty Smith',
+                    image:'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+                },
+                {
+                    name:'Summer Smith',
+                    image:'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+                },
+                {
+                    name:'Beth Smith',
+                    image:'https://rickandmortyapi.com/api/character/avatar/4.jpeg'
+                },
+                {
+                    name:'Jerry Smith',
+                    image:'https://rickandmortyapi.com/api/character/avatar/5.jpeg'
+                }
+            ]
+        }
+    },
+
+}; 
+    
+</script>
+
+
+<style scoped>
+
+.bg{
+    background: url("https://rickandmortypod.com/wp-content/uploads/2018/11/cropped-RM_page-header_background1-3.png") no-repeat;
+    background-size: cover;
+    height: 100vh;
+    padding: 20px;
+}
+</style>
---
+++ src/TarjetaComponente.vue
@@ -0,0 +1,35 @@
+<template>
+
+    <div class="tarjeta">
+        <img :src= 'imagen'/>
+        <p>{{nombre}}</p>
+    </div>
+
+</template>
+<script>
+    export default {
+    name: "tarjetaComponente",
+     props: {
+        nombre: String,
+        imagen: String
+      },
+};
+
+</script>
+
+<style scoped>
+    .tarjeta{
+        display: flex;
+        align-items: center;
+        padding: 10px 50px;
+    }
+    .tarjeta img{
+        border-radius: 50%;
+        width: 100px;
+    }
+    .tarjeta p{
+        font-size: 30px;
+        color: green;
+        margin-left: 30px;
+    }
+    </style>
